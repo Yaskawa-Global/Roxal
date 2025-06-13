75db6fa84a6f63644bb6c4fb8d429be43f5d4b96
David Jung <davidljung@users.noreply.github.com>
2025-06-12 23:54:52 -0500

    Add actor cloning tests

--- runtests.py
+++ runtests.py
@@ -22,7 +22,7 @@ tests = [
     'importmodule1', 'importstar', 'importsyms', 'importdiamond',
     'method_named_param',
     'annot1', 'generic', 'objscopes',
-    'threads1', 'actor1', 'actor2', 'actor3', 'actor4', 'actor5', 'actor6',
+    'threads1', 'actor1', 'actor2', 'actor3', 'actor4', 'actor5', 'actor6', 'actor7', 'actor8',
     'clone1', 'extends1', 'nothis', 'superprop', 'scopetest4'
 ]
 
