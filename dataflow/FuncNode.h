02f17f1ab1302d69566687217b9966f21e76339c
David Jung <davidljung@users.noreply.github.com>
2025-06-18 14:12:45 -0500

    Phase II Step 1: detect signal args in calls (#60)
    * Rename RoxalFunc to FuncNode and map const args
    
    * Add closure execution for FuncNode

---
+++ dataflow/FuncNode.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Func.h"
+#include "compiler/Object.h"
+#include "compiler/Value.h"
+
+namespace df {
+
+class FuncNode : public Func
+{
+public:
+    using ConstArgMap = std::map<std::string, roxal::Value>;
+
+    FuncNode(const std::string& name,
+             roxal::ObjClosure* closure,
+             const ConstArgMap& constArgs,
+             const std::vector<ptr<Signal>>& signalArgs);
+
+    virtual Names inputNames() const override { return m_inputNames; }
+    virtual Names outputNames() const override { return {}; }
+    virtual Values operator()(const Values& inputValues) override;
+
+    roxal::ObjClosure* closure;
+    ConstArgMap constArgs;
+    std::vector<ptr<Signal>> signalArgs;
+
+    // parameter names in order of declaration
+    std::vector<std::string> paramNames;
+    // index of signal argument for each param (-1 if constant)
+    std::vector<int> paramSignalIndex;
+
+private:
+    Names m_inputNames;
+};
+
+}
