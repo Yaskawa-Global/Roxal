6902331c1259f6110482074e9720306d48f054f5
David Jung <417058+davidljung@users.noreply.github.com>
2025-07-04 15:10:27 -0500

    actor_closure* test tweaks

--- runtests.py
+++ runtests.py
@@ -44,9 +44,7 @@ tests = [
     'threads1', 'fork_upvalue_error', 'fork_no_upvalues', 
     'actor1', 'actor2', 'actor3', 'actor4', 'actor5', 'actor6', 'actor7', 'actor8', 'actor9', 
     'actor_init', 'actor_stack',
-    #'actor_closure1',
-    'actor_closure2', # hangs
-    'actor_closure3', # currently leaks
+    'actor_closure1', 'actor_closure2', 'actor_closure3',
     'clone1', 'extends1', 'nothis', 'superprop', 'scopetest4',
     'private_prop', 'private_method', 'private_inherit',
     'typededucer_binop', 'typededucer_ops',
