17c546528e94d59e922712106b7acd9fdb0eda4e
David Jung <davidljung@users.noreply.github.com>
2025-07-16 00:40:30 -0500

    Fix isPrimitive for byte and enum

--- runtests.py
+++ runtests.py
@@ -37,7 +37,7 @@ tests = [
     'nonstrict-assign', 'nonstrict-assign-err', 'strict-assign', 'strict-assign-err',
     'module_strict_assign_err', 'func_nonstrict', 'conversions1',
     'serialize_values', 'serialize_objects', 'serialize_user_objects', 'serialize_func', 'serialize_actor',
-    'byteops', 'bitwise', 'byte_int_bits', 'object_init', 'object_inherit_is',
+    'byteops', 'bitwise', 'byte_int_bits', 'list_byte_concat', 'list_enum_concat', 'object_init', 'object_inherit_is',
     'closure', 'closure2', 'closure3', 'closure4', 'closure5', 'closure_many', 'lambda1',
     'conversion1', 'string_interp',
     'call_param_type_nonstrict', 'call_param_type_strict', 'param_assign_static_err',
