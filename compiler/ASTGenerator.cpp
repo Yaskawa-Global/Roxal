6385ce80820bccb338d0104608a65b4e6e6231a9
David Jung <davidljung@users.noreply.github.com>
2025-07-18 15:33:45 -0500

    Improve parse error reporting

--- compiler/ASTGenerator.cpp
+++ compiler/ASTGenerator.cpp
@@ -209,6 +209,22 @@ ptr<AST> ASTGenerator::ast(std::istream& source, const std::string& name)
     RoxalIndentationLexer lexer(&input);
     CommonTokenStream tokens(&lexer);
 
+    class ParserErrorListener : public antlr4::BaseErrorListener {
+    public:
+        bool hadError = false;
+        virtual void syntaxError(antlr4::Recognizer *recognizer,
+                                 antlr4::Token *offendingSymbol,
+                                 size_t line, size_t charPositionInLine,
+                                 const std::string &msg, std::exception_ptr e) override
+        {
+            if (!hadError) {
+                hadError = true;
+                compileError(std::to_string(line) + ":" +
+                             std::to_string(charPositionInLine) + " - " + msg);
+            }
+        }
+    } errorListener;
+
     #if defined(DEBUG_OUTPUT_LEXER_TOKENS)
     std::cout << "== tokens ==" << std::endl;
     tokens.fill();
@@ -222,8 +238,18 @@ ptr<AST> ASTGenerator::ast(std::istream& source, const std::string& name)
     #endif
 
     RoxalParser parser(&tokens);
+    lexer.removeErrorListeners();
+    parser.removeErrorListeners();
+    lexer.addErrorListener(&errorListener);
+    parser.addErrorListener(&errorListener);
 
     auto tree = parser.file_input();
+
+    if (errorListener.hadError) {
+        this->source = nullptr;
+        this->sourceName.clear();
+        return nullptr;
+    }
 
     #if defined(DEBUG_OUTPUT_PARSE_TREE)
     std::cout << "== parse tree ==" << std::endl << tree::Trees::toStringTree(tree,&parser,true) << std::endl;
