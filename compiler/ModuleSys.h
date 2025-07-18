c9ec7e58aaf968b1f62e295f0da61b090a404d85
David Jung <davidljung@users.noreply.github.com>
2025-07-18 13:41:07 -0500

    Move sys builtins into ModuleSys (#260)

--- compiler/ModuleSys.h
+++ compiler/ModuleSys.h
@@ -15,6 +15,36 @@ public:
 
     inline ObjModuleType* moduleType() const { return asModuleType(moduleTypeValue); }
 
+    // builtin function implementations
+    Value print_builtin(VM& vm, int argCount, Value* args);
+    Value len_builtin(VM& vm, int argCount, Value* args);
+    Value clone_builtin(VM& vm, int argCount, Value* args);
+    Value wait_builtin(VM& vm, int argCount, Value* args);
+    Value fork_builtin(VM& vm, int argCount, Value* args);
+    Value join_builtin(VM& vm, int argCount, Value* args);
+    Value stacktrace_builtin(VM& vm, int argCount, Value* args);
+    Value threadid_builtin(VM& vm, int argCount, Value* args);
+    Value stackdepth_builtin(VM& vm, int argCount, Value* args);
+    Value await_builtin(VM& vm, int argCount, Value* args);
+    Value runtests_builtin(VM& vm, int argCount, Value* args);
+    Value weakref_builtin(VM& vm, int argCount, Value* args);
+    Value weak_alive_builtin(VM& vm, int argCount, Value* args);
+    Value strongref_builtin(VM& vm, int argCount, Value* args);
+    Value serialize_builtin(VM& vm, int argCount, Value* args);
+    Value deserialize_builtin(VM& vm, int argCount, Value* args);
+    Value toJson_builtin(VM& vm, int argCount, Value* args);
+    Value fromJson_builtin(VM& vm, int argCount, Value* args);
+
+    // native implementations
+    Value clock_native(VM& vm, int argCount, Value* args);
+    Value clock_signal_native(VM& vm, int argCount, Value* args);
+    Value signal_source_native(VM& vm, int argCount, Value* args);
+    Value engine_stop_native(VM& vm, int argCount, Value* args);
+    Value typeof_native(VM& vm, int argCount, Value* args);
+    Value df_graph_native(VM& vm, int argCount, Value* args);
+    Value df_graphdot_native(VM& vm, int argCount, Value* args);
+    Value loadlib_native(VM& vm, int argCount, Value* args);
+
 private:
     Value moduleTypeValue; // ObjModuleType*
 };
