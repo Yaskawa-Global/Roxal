871808623852f27681244009ef870d0a6e0024a7
David Jung <417058+davidljung@users.noreply.github.com>
2025-07-21 16:56:29 -0500

    removed char* versions

--- compiler/Object.h
+++ compiler/Object.h
@@ -1058,12 +1058,11 @@ struct ObjectInstance : public Obj
     ObjObjectType* instanceType;
     std::unordered_map<int32_t, Value> properties;
 
+    // convenience methods for property access (e.g. for builtin method implementations)
     Value getProperty(const icu::UnicodeString& name) const;
     Value getProperty(const std::string& name) const { return getProperty(toUnicodeString(name)); }
-    Value getProperty(const char* name) const { return getProperty(std::string(name)); }
     void setProperty(const icu::UnicodeString& name, Value value);
     void setProperty(const std::string& name, Value value) { setProperty(toUnicodeString(name), value); }
-    void setProperty(const char* name, Value value) { setProperty(std::string(name), value); }
 
     void write(std::ostream& out, roxal::ptr<SerializationContext> ctx = nullptr) const override;
     void read(std::istream& in, roxal::ptr<SerializationContext> ctx = nullptr) override;
