e33767af417b5aba85fb2fa56246aa17ccf487db
David Jung <417058+davidljung@users.noreply.github.com>
2026-07-20 13:17:11 -0500

    Bumped module cache file version

--- compiler/RoxalCompiler.cpp
+++ compiler/RoxalCompiler.cpp
@@ -29,7 +29,7 @@ using ast::Access;
 namespace {
 
 constexpr char ModuleCacheMagic[4] = {'R', 'O', 'X', 'C'};
-constexpr std::uint32_t ModuleCacheVersion = 46;
+constexpr std::uint32_t ModuleCacheVersion = 47;
 
 std::filesystem::path moduleCachePathFor(const std::filesystem::path& sourcePath) {
     if (sourcePath.empty())
